b5d374538f7af6d90ce754b5f5a689d4b3d21ddc
monukumar98 <monu012as@gmail.com>
2025-02-08 19:21:57 +0530

    Recursion and Backtracking

--- Lec18/Key_Paid.java
+++ Lec18/Key_Paid.java
@@ -10,18 +10,20 @@ public class Key_Paid {
 		// TODO Auto-generated method stub
 		String ques = "23";
 		List<String> ll = new ArrayList<>();
-		KeyPaid(ques, "",ll);
+		KeyPaid(ques, "", ll);
+		System.out.println(ll);
 	}
 
-	public static void KeyPaid(String ques, String ans,List<String> ll) {
+	public static void KeyPaid(String ques, String ans, List<String> ll) {
 		if (ques.length() == 0) {
-			System.out.println(ans);
+			// System.out.println(ans);
+			ll.add(ans);
 			return;
 		}
 		char ch = ques.charAt(0);
 		String s = key[ch - '0'];// abc
 		for (int i = 0; i < s.length(); i++) {
-			KeyPaid(ques.substring(1), ans + s.charAt(i),ll);
+			KeyPaid(ques.substring(1), ans + s.charAt(i), ll);
 		}
 
 	}
