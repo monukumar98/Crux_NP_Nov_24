e5a462457341ad29c9c5d6ecaf76db7e11c8ed94
monukumar98 <monu012as@gmail.com>
2025-03-01 19:07:00 +0530

    D&C

---
+++ Lec23/Merge_Sort.java
@@ -0,0 +1,56 @@
+package Lec23;
+
+public class Merge_Sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = { 7, 5, 3, 4, 2 };
+		int [] ans =Sort(arr, 0, arr.length-1);
+		for (int i = 0; i < ans.length; i++) {
+			System.out.print(ans[i]+" ");
+		}
+
+	}
+
+	public static int[] Sort(int[] arr, int si, int ei) {
+		if(si==ei) {
+			int [] b = new int [1];
+			b[0]=arr[si];
+			return b;
+		}
+		int mid = (si + ei) / 2;
+		int[] f = Sort(arr, si, mid);
+		int[] s = Sort(arr, mid + 1, ei);
+		return Merge(f, s);
+
+	}
+
+	public static int[] Merge(int[] arr1, int[] arr2) {
+		int n = arr1.length;
+		int m = arr2.length;
+		int[] ans = new int[n + m];
+		int i = 0, j = 0, k = 0;
+		while (i < n && j < m) {
+			if (arr1[i] < arr2[j]) {
+				ans[k] = arr1[i];
+				i++;
+				k++;
+			} else {
+				ans[k] = arr2[j];
+				j++;
+				k++;
+			}
+		}
+		while (i < n) {
+			ans[k] = arr1[i];
+			i++;
+			k++;
+		}
+		while (j < m) {
+			ans[k] = arr2[j];
+			j++;
+			k++;
+		}
+		return ans;
+	}
+}
---
+++ Lec23/Power.java
@@ -0,0 +1,24 @@
+package Lec23;
+
+public class Power {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 3;
+		int n = 5;
+		System.out.println(pow(a, n));
+	}
+
+	public static int pow(int a, int n) {
+		if (n == 0) {
+			return 1;
+		}
+		int ans = pow(a, n / 2);
+		ans = ans * ans;
+		if (n % 2 != 0) {
+			ans *= a;
+		}
+		return ans;
+	}
+
+}
