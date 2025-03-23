4d53707102c7ed1575bcf1aab86cfc97e57f20bb
monukumar98 <monu012as@gmail.com>
2025-03-23 19:13:54 +0530

    Stack

---
+++ Lec27/Histogram.java
@@ -0,0 +1,43 @@
+package Lec27;
+
+import java.util.Stack;
+
+public class Histogram {
+
+	public static void main(String[] args) {
+		int[] arr = { 2, 1, 5, 6, 2, 3 };
+		System.out.println(largestArea(arr));
+	}
+
+	public static int largestArea(int[] arr) {
+		int area = 0;
+		Stack<Integer> st = new Stack<>();
+		for (int i = 0; i < arr.length; i++) {
+			while (!st.isEmpty() && arr[i] < arr[st.peek()]) {
+				int h = arr[st.pop()];
+				int r = i;
+				if (st.isEmpty()) {
+					area = Math.max(area, h * r);
+				} else {
+					int l = st.peek();
+					area = Math.max(area, h * (r - l - 1));
+				}
+			}
+			st.push(i);
+		}
+		int r = arr.length;
+		while (!st.isEmpty()) {
+			int h = arr[st.pop()];
+
+			if (st.isEmpty()) {
+				area = Math.max(area, h * r);
+			} else {
+				int l = st.peek();
+				area = Math.max(area, h * (r - l - 1));
+			}
+		}
+		return area;
+
+	}
+
+}
