2ae088ba2c014f496ec1f9ea1895ce3f59cd3987
monukumar98 <monu012as@gmail.com>
2025-03-29 17:30:56 +0530

    Stack Qps

--- Lec27/Maximal_Rectangle.java
+++ Lec27/Maximal_Rectangle.java
@@ -1,9 +1,56 @@
 package Lec27;
 
+import java.util.Stack;
+
 public class Maximal_Rectangle {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		char[][] matrix = { { '1', '0', '1', '0', '0' }, { '1', '0', '1', '1', '1' }, { '1', '1', '1', '1', '1' },
+				{ '1', '0', '0', '1', '0' } };
+		int[] arr = new int[matrix[0].length];
+		int ans = 0;
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				if (matrix[i][j] == '1') {
+					arr[j]++;
+				} else {
+					arr[j] = 0;
+				}
+			}
+			ans = Math.max(ans, largestArea(arr));
+		}
+		System.out.println(ans);
+	}
+
+	public static int largestArea(int[] arr) {
+		int area = 0;
+		Stack<Integer> st = new Stack<>();
+		for (int i = 0; i < arr.length; i++) {
+			while (!st.isEmpty() && arr[i] < arr[st.peek()]) {
+				int h = arr[st.pop()];
+				int r = i;
+				if (st.isEmpty()) {
+					area = Math.max(area, h * r);
+				} else {
+					int l = st.peek();
+					area = Math.max(area, h * (r - l - 1));
+				}
+			}
+			st.push(i);
+		}
+		int r = arr.length;
+		while (!st.isEmpty()) {
+			int h = arr[st.pop()];
+
+			if (st.isEmpty()) {
+				area = Math.max(area, h * r);
+			} else {
+				int l = st.peek();
+				area = Math.max(area, h * (r - l - 1));
+			}
+		}
+		return area;
 
 	}
 
